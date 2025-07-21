697062d26fc74f630ac8439f968a16b0e851881e
Tiasa Mahanty <tiasamahanty03@gmail.com>
2025-07-21 20:52:38 +0530

    first commit

---
+++ E-Learning-Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black text-white w-screen min-h-screen flex items-center justify-center'>
+      app
+    </div>
+  )
+}
+
+export default App
