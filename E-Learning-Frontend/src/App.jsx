1c8b8c13151149fa9450ef92a0422ec4b05cf550
Tiasa Mahanty <tiasamahanty03@gmail.com>
2025-07-22 00:39:52 +0530

    Change in app

--- E-Learning-Frontend/src/App.jsx
+++ E-Learning-Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='bg-black text-white w-screen min-h-screen flex items-center justify-center'>
-      appppppppp
+      ap
     </div>
   )
 }
