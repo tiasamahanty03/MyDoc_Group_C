e6089dc85af186a1932504f570c75f20f858db56
Swagnik <swagnikghosh2002@gmail.com>
2025-08-09 20:16:15 +0530

    databaSE CONNECTION

--- MyDoc-Backend/server.js
+++ MyDoc-Backend/server.js
@@ -1,11 +1,14 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+
 
 
 // app config
 const app = express()
 const port = process.env.PORT || 4000
+await connectDB()
 
 // middlewares
 app.use(express.json())
