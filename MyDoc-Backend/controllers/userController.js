ef118eec70cb3bc0985d92b2449750d39da48657
Ritam Chakraborty <chakrabortyritam29@gmail.com>
2025-08-12 19:57:33 +0530

    Fixed get profile issue

--- MyDoc-Backend/controllers/userController.js
+++ MyDoc-Backend/controllers/userController.js
@@ -70,15 +70,15 @@ const loginUser = async (req,res) => {
 
 
 // API to get user profile data
-const getProfile = async (req,res)=>{
-    try {
-        const { userId } = req.body
-        const userData = await userModel.findById(userId).select('-password')
+const getProfile = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const userData = await userModel.findById(userId).select("-password");
 
-        res.json({success:true,userData})
-    } catch (error) {
-        console.log(error)
-        res.json({success:false, message: error.message})
-    }
-}
+    res.json({ success: true, userData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 export {registerUser,loginUser,getProfile}
--- MyDoc-Backend/routes/userRoute.js
+++ MyDoc-Backend/routes/userRoute.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { registerUser,loginUser, getProfile } from '../controllers/userController.js'
+import authUser from '../middlewares/authUser.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 
-userRouter.get('/get-profile',authUser,getProfile)
+userRouter.get('/get-profile',authUser,getProfile)  
 
 
 
