ccbf850cfb9a85cfd5b7aa0ae5e00d0b2c3e85c8
Tiasa Mahanty <tiasamahanty03@gmail.com>
2025-08-13 20:14:56 +0530

    fixing some errors

--- MyDoc-Backend/controllers/userController.js
+++ MyDoc-Backend/controllers/userController.js
@@ -10,6 +10,7 @@ const registerUser = async (req,res) => {
     try {
         const {name, email, password} = req.body
 
+
         if ( !name || !password || !email) {
             return res.json({success:false,message:"Missing Details"})
         }
